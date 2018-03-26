12fb164149b7633b5252379f57974161ce2b3400
Playingtygre <kymiehsu123@gmail.com>
2018-03-25 19:43:31 -0700

    testing

--- Code_Challenges/LargestProduct/Largest_product/XUnitTestProject_largestProduct/UnitTest1.cs
+++ Code_Challenges/LargestProduct/Largest_product/XUnitTestProject_largestProduct/UnitTest1.cs
@@ -21,6 +21,38 @@ namespace XUnitTestProject_largestProduct
 
             Assert.Equal(25, Program.LargestProduct(inputArray));
         }
+
+        [Fact]
+        public void Test2()
+        {
+            int[,] inputArray = new int[,]
+            {
+                { 1, 2 },
+                { 3, 4 },
+                { 5, 6 },
+                { 7, 8 },
+                { 9, 10 }
+            };
+
+            Assert.Equal(90, Program.LargestProduct(inputArray));
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            int[,] inputArray = new int[,]
+            {
+                { 1, 1 },
+                { 2, 2 },
+                { 3, 3 },
+                { 4, 4 },
+                { 5, 5 }
+            };
+
+            Assert.Equal(25, Program.LargestProduct(inputArray));
+        }
+
+
     }
 
 
