73a312223a2d6a6ffb2da1057d312402b155a96a
Playingtygre <kymiehsu123@gmail.com>
2018-03-25 19:26:10 -0700

    Method to loop thru a 2D array

--- Code_Challenges/LargestProduct/Largest_product/Largest_product/Program.cs
+++ Code_Challenges/LargestProduct/Largest_product/Largest_product/Program.cs
@@ -1,13 +1,41 @@
 ﻿using System;
 
-
-namespace Largest_product
+namespace largest_product
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            int[,] sampleArray = new int[,] { { 1, 2 }, { 3, 4 }, { 5, 6 }, { 7, 8 } };
+            string answer = LargestProduct(sampleArray).ToString();
+            Console.WriteLine(answer);
+            Console.ReadKey();
         }
+
+
+        public static int LargestProduct(int[,] arr)
+        {
+            //declaring the product to zero
+            int product = 0;
+
+            //iterates thru the first loop to find all the integers 
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                // iterates thru the secont loop to find the second number
+                for (int a = 0; a < arr.GetLength(1) - 1; a++)
+                {
+                   //if else statement if current [a,i] are compared with prevous loop
+                    if (arr[i, a] * arr[i, a + 1] > product)
+                    {
+                        // since the next set of numbers is larger change product.
+                        product = arr[i, a] * arr[i, a + 1];
+                    }
+
+                }
+            }
+            return product;
+        }
+
     }
 }
