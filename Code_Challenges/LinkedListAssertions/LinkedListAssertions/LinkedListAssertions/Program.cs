44ccabbc458c38fded9152805cf2d0f519582a73
Playingtygre <kymiehsu123@gmail.com>
2018-03-27 13:46:31 -0700

    added ll

---
+++ Code_Challenges/LinkedListAssertions/LinkedListAssertions/LinkedListAssertions/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LinkedListAssertions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
