0915722f5bd1ec4884a366f205d9aa4f4452d29a
Playingtygre <kymiehsu123@gmail.com>
2018-03-27 20:47:29 -0700

    Notes,

---
+++ Code_Challenges/11_kth-from-end/11_kth_from_end/11_kth_from_end/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _11_kth_from_end
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is the K_nth");
+
+            //**Remember to declare current.Node -> head
+            // While Current NOTEQUAL null
+            //Current Node <-- Current.Next 
+        }
+    }
+}
