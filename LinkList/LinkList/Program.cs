f59ab9fc65b4d8a7617036c906f674063c74c331
Playingtygre <kymiehsu123@gmail.com>
2018-03-27 21:02:21 -0700

    Linklist

---
+++ LinkList/LinkList/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LinkList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+            // watch videos from 3/23/18 this will give you a better understanding 
+        }
+    }
+}
