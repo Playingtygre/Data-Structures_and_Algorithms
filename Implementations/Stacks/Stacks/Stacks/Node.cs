1e65b26abaf84a86545396f696ff6f2042814e38
Playingtygre <kymiehsu123@gmail.com>
2018-03-31 09:13:32 -0700

    Stating off poing with Stacks an Quest examples given in class

---
+++ Implementations/Stacks/Stacks/Stacks/Node.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stacks
+{
+    class Node
+    {
+        // adding the abstraction Node class, Value and Next
+        public int Value { get; set; }
+        public Node Next { get; set; }
+    }
+}
