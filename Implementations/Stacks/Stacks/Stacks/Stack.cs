c9b2d9f3873ea7911cb71455b3078747f18a63d7
Playingtygre <kymiehsu123@gmail.com>
2018-04-02 10:43:19 -0700

    adding pop method

--- Implementations/Stacks/Stacks/Stacks/Stack.cs
+++ Implementations/Stacks/Stacks/Stacks/Stack.cs
@@ -7,6 +7,7 @@ namespace Stacks
 
     public class Stack
     {
+
         public Node Head { get; set; }
         public Stack(Node node)
         {
@@ -19,12 +20,17 @@ namespace Stacks
             node.Next = Head;
             Head = node;
         }
-        //
+
+        //pulling a value from the top of the stack
         public void Pop(Node node)
         {
-
-            node.Next = Head;
-            Head = node;
+            if (Head == null)
+            {
+                Console.WriteLine("you are at the bottom of the stack");
+                return;
+            }
+            Console.WriteLine("Item popped is {0}", Head.Next);
+            Head = node.Next;
         }
 
         public Node Peek() => Head;
