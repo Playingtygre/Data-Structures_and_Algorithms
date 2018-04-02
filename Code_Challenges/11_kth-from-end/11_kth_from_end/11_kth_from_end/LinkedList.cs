53ebfb8f5c408ebea46624f0aa850e2f4d2d8195
Playingtygre <kymiehsu123@gmail.com>
2018-04-01 23:25:13 -0700

    Node adding

---
+++ Code_Challenges/11_kth-from-end/11_kth_from_end/11_kth_from_end/LinkedList.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace kth_from_end
+{
+    public class LinkedList
+    {
+        public Node Head { get; set; }
+        public LinkedList(int value)
+        {
+            Head = new Node(value);
+        }
+
+        //method to add an array of values to a linked list
+        public LinkedList(int [] values)
+        {
+            LinkedList l = new LinkedList(values[0]);
+            for (int i = 1; i < values.Length; i++)
+            {
+                l.Add(values[i]);
+            }
+            Head = l.Head;
+        }
+        //method to add values to nodes
+        public void Add(int value)
+        {
+            Node addition = new Node(value);
+            Node old = Head;
+            addition.Next = old;
+            Head = addition;
+        }
+
+        // method to find a value by looking for and index
+        public int Find(int value)
+        {
+            int index = 0;
+            Node current = Head;
+            do
+            {
+                if (current.Value == value)
+                {
+                    return index;
+                }
+                index++;
+                current = current.Next;
+            } while (current.Next != null);
+            return -1;
+        }
+
+        //
+        public int Length()
+        {
+            Node current = Head;
+            int count = 1;
+            while (current)
+        }
+
+
+
+    }
+}
