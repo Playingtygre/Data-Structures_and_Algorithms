a08e24e9f82c24bda578776b90190cb123798c96
Playingtygre <kymiehsu123@gmail.com>
2018-04-07 11:30:23 -0700

    Chaninge Folders

--- Code_Challenges/11_kth-from-end/11_kth_from_end/11_kth_from_end/LinkedList.cs
+++ Code_Challenges/11_kth-from-end/11_kth_from_end/11_kth_from_end/LinkedList.cs
@@ -2,80 +2,62 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace kth_from_end
+namespace Linked_List
 {
-    public class LinkedList
+    public class SinglyLinked
     {
         public Node Head { get; set; }
-        public LinkedList(int value)
+
+        public SinglyLinked(Node node)
         {
-            Head = new Node(value);
+            Head = node;
         }
 
-        //method to add an array of values to a linked list
-        public LinkedList(int [] values)
+        public void PrintAllNodes()
         {
-            LinkedList l = new LinkedList(values[0]);
-            for (int i = 1; i < values.Length; i++)
+            Node cur = Head;
+                             
+            while (cur.Next != null)
             {
-                l.Add(values[i]);
+                Console.WriteLine($"This is the {cur.Value}Current Value ");
+                cur = cur.Next;
             }
-            Head = l.Head;
-        }
-        //method to add values to nodes
-        public void Add(int value)
-        {
-            Node addition = new Node(value);
-            Node old = Head;
-            addition.Next = old;
-            Head = addition;
+            Console.WriteLine($"cur.Value: {cur.Value}");
         }
 
-        // method to find a value by looking for and index
-        public int Find(int value)
+        public Node kthFromEnd(int k)
         {
-            int index = 0;
-            Node current = Head;
-            do
+            Node cur = Head; 
+
+            try
             {
-                if (current.Value == value)
+                int length = 0; 
+                while (cur.Next != null)
                 {
-                    return index;
+                   
+                    length++;
+                    cur = cur.Next; 
                 }
-                index++;
-                current = current.Next;
-            } while (current.Next != null);
-            return -1;
-        }
-
-        //Method to count the entire length of the Node using a
-        //for loop
-        public int Length()
-        {
-            Node current = Head;
-            int count = 1;
-            while (current.Next != null)
-            {
-                count++;
-                current = current.Next;
+                length++; 
+               
+                cur = Head; 
+                for (int i = 0; i < (length - k - 1); i++)
+                {
+                    cur = cur.Next;
+                }
+                if (k > length)
+                    throw new System.ArgumentException("k is greater than length of list");
+                return cur;
             }
-            return count;
-        }
-        //method for finding kth postion
-        public Node Kth(int k)
-        {
-            //setting up the lenght subtract to prevent from going to null
-            int len = Length() - 1;
-            // seting the current node at the start
-            Node current = Head;
-            //itterate thru the nodes until current matches with next node.
-            for (int i = 0; i < len - k; i++)
-            {
-                current = current.Next;
-            }
-            return current;
-        }
 
+            catch (Exception)
+            {
+
+                Console.WriteLine("k is greater than length of list");
+            }
+            return cur;
+
+        }
 
     }
 }
--- Code_Challenges/11_kth-from-end/11_kth_from_end/11_kth_from_end/Program.cs
+++ Code_Challenges/11_kth-from-end/11_kth_from_end/11_kth_from_end/Program.cs
@@ -1,29 +1,76 @@
-﻿using System;
+﻿using Linked_List;
+using System;
+using System.Collections.Generic;
 
-namespace _11_kth_from_end
+namespace Linked_List
 {
     class Program
+
+    /* Part of this code were given by Amanda Iverson in example lecuture
+     *  Parts of this code were copied from Aurthur Allen
+     *  Parts of this code were collaborated with Arthur Allen
+     */ 
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("This is the K_nth");
+            Console.WriteLine("Hello World!, This is our Kth in the List");
 
-            //**Remember to declare current.Node -> head
-            // While Current NOTEQUAL null
-            //Current Node <-- Current.Next 
+            // Declare the nodes
+            Node node1 = new Node();
+            Node node2 = new Node();
+            Node node3 = new Node();
+            Node node4 = new Node();
+            Node node5 = new Node();
+            Node node6 = new Node();
+            Node node7 = new Node();
 
-            
+            // Link the nodes
+            node1.Next = node2;
+            node2.Next = node3;
+            node3.Next = node4;
+            node4.Next = node5;
+            node5.Next = node6;
+            node6.Next = node7;
+            node7.Next = null;
 
-            
-        }
+            // Populate the nodes
+            node1.Value = 1;
+            node2.Value = 3;
+            node3.Value = 8;
+            node4.Value = 2;
+            node5.Value = 2;
+            node6.Value = 2;
+            node7.Value = 2;
 
-        public static void LL()
-        {
-           
-            Console.WriteLine();
+            // Instantiate the list
+            SinglyLinked sll = new SinglyLinked(node1);
+
+            Console.WriteLine("this is our singly-linked node list");
+            Console.WriteLine("[1][3][8][7][6][5][9] -> HEAD");
+            Console.WriteLine("Go ahead and type in a number to check its position");
+
+            int number;
+
+            ConsoleKeyInfo UserInput = Console.ReadKey();
+            if (char.IsDigit(UserInput.KeyChar))
+            {
+                number = int.Parse(UserInput.KeyChar.ToString());
+            }
+            else
+            {
+                number = 0;
+            }
+
+            int k = number;
+
+            Node node = sll.kthFromEnd(k);
+            Console.WriteLine($"{k} is the {node.Value} node from the HEAD");
+            //Console.WriteLine($"{node.Value} is the {k}th node from the HEAD");
+
             Console.ReadLine();
-
-
         }
+
+
+
     }
 }
