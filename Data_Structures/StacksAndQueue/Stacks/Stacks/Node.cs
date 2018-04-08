61d6e6eb2987771e5f7d6d3c92a6219544bd86ff
Playingtygre <kymiehsu123@gmail.com>
2018-04-07 21:00:41 -0700

    adding better comments

--- Data_Structures/StacksAndQueue/Stacks/Stacks/Node.cs
+++ Data_Structures/StacksAndQueue/Stacks/Stacks/Node.cs
@@ -6,7 +6,7 @@ namespace Stacks
 {
     public class Node
     {
-        // adding the abstraction Node class, Value and Nextt
+        // adding the abstraction Node class, value and Next Node
         public int Value { get; set; }
         public Node Next { get; set; }
         public Node Tail { get; set; }
