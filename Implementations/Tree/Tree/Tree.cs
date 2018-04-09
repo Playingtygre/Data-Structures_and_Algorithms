33864b861a3b0177acf10cbfd65362d9edfa5219
Playingtygre <kymiehsu123@gmail.com>
2018-04-09 09:35:24 -0700

    Building out a tree

---
+++ Implementations/Tree/Tree/Tree.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tree
+{
+    class Tree
+    {
+        public Node Root { get; set; }
+
+        //remember Root is node 
+
+        public Tree(Node node)
+        {
+            Root = node;
+        }
+
+        // THS IS THE TRAVERSAL LIST
+        public void InOrder(Node node)
+        {
+
+            if (node.LeftChild != node)
+            {
+                InOrder(node.LeftChild);
+            }
+
+
+            Console.WriteLine(node.Value);
+
+            if (node.RightChild != null)
+            {
+                InOrder(node.RightChild);
+            }
+
+        }
+
+        // THIS IS THE TRAVERSAL LIST  
+        public void PreOrder(Node node)
+        {
+            if (node == null)
+            {
+                return;
+            }
+            Console.WriteLine(node.Value);
+
+            if (node.LeftChild != null)
+            {
+
+            }
+
+
+
+            if (node.RightChild != node)
+            {
+                PreOrder(node.RightChild);
+            }
+
+        }
+
+        
+
+
+
+
+    }
+}
