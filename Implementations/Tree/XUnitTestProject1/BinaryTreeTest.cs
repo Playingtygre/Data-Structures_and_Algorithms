104cc5af61af4920cb2cf2e5c3a498ec2e39fc13
Playingtygre <kymiehsu123@gmail.com>
2018-04-29 15:12:15 -0700

    USING BETTER EXAMPLES

---
+++ Implementations/Tree/XUnitTestProject1/BinaryTreeTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BinaryTree;
+using BinaryTreeTest;
+using Xunit;
+
+namespace XUnitTestProject1
+{
+   public  class BinaryTreeTest
+    {
+
+        [Theory]
+        [ClassData(typeof(TreeTestData))]
+        public void PreOrderTest(Tree<int> data)
+        {
+            // Arrange
+            // Reference TreeTestData.GetEnumerator()
+            List<int> expectedValues = new List<int> { 1, 2, 3, 8, 9, 4, 5, 6, 7 };
+            List<int> actualValues = new List<int>();
+
+            // Act
+            actualValues = data.PreOrderTraversal(data.Root, actualValues);
+
+            // Assert
+            Assert.Equal(expectedValues, actualValues);
+        }
+    }
+}
