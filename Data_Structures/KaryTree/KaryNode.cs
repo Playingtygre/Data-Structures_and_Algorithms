27547a287a5543629c89ecd379d744ad87f56030
Playingtygre <kymiehsu123@gmail.com>
2018-04-29 17:59:34 -0700

    Attempting

---
+++ Data_Structures/KaryTree/KaryNode.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace K_aryTrees
+{
+    /*  Node <T> will be a generics list
+     */
+
+    public class Node<T>
+    {
+        
+        /* Knary Tree; Root node has children,
+         */
+
+        public Node()
+        {
+            // Setting up Constuctor to the generics list.
+            Children = new List<Node<T>>();
+        }
+
+        /*  Node with an argument of generics list, its children will be
+         *  the generics list. Setting
+        */
+        public Node(T value)
+        {
+            Children = new List<Node<T>>();
+            Value = value;
+        }
+
+        /* An public node with all the children, this will take in a parameter
+         * with array of T and its children.
+         */
+
+        public Node(T value, T[] children)
+        {
+            Children = new List<Node<T>>();
+            foreach (T val in children)
+            {
+                Children.Add(new Node<T>(val));
+            }
+            Value = value;
+        }
+
+    
+        public T Value { get; set; }
+        public List<Node<T>> Children { get; set; }
+    }
+}
---
+++ Data_Structures/KaryTree/KaryTree.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections.Generic;
+
+namespace K_aryTrees
+{
+    /*  Generics T tree
+     */
+
+    public class Tree<T>
+    {
+
+        /* This is the knary tree construtor 
+         */
+        // Setting up the generics <T > root 
+
+        public Tree(Node<T> root)
+        {
+            //Root.<T> to Node.root
+            Root = root;
+        }
+
+       
+
+        //<T>root 
+        public Tree(T rootValue)
+        {
+            //<T>.Root value set as value
+            Root = new Node<T>() { Value = rootValue };
+        }
+
+
+        /* <T>.Root set
+         */
+
+        /* Setting up the properties of the <T> 
+           */
+
+        public Node<T> Root { get; set; }
+
+        //
+        public delegate void Method(Node<T> n);
+
+        /*Traversing recursively
+         */
+
+        public void PreOrderTraverse(Method func)
+        {
+            func(Root);
+            foreach (Node<T> n in Root.Children)
+            {
+                Tree<T> t = new Tree<T>(n);
+                t.PreOrderTraverse(func);
+            }
+        }
+
+        public void PostOrderTraverse(Method func)
+        {
+            foreach (Node<T> n in Root.Children)
+            {
+                Tree<T> t = new Tree<T>(n);
+                t.PreOrderTraverse(func);
+            }
+            func(Root);
+        }
+
+
+        //Using queue for a for a traversal
+        public void BreadthFirstTraverse(Method func)
+        {
+            Queue<Node<T>> q = new Queue<Node<T>>();
+            q.Enqueue(Root);
+            Node<T> current;
+            while (q.TryDequeue(out current))
+            {
+                func(current);
+                foreach (Node<T> n in current.Children)
+                {
+                    q.Enqueue(n);
+                }
+            }
+        }
+
+        public void Add(T target, T addition)
+        {
+            bool added = false;
+            void AddConditionally(Node<T> current)
+            {
+                if (!added && current.Value.Equals(target))
+                {
+                    current.Children.Add(new Node<T> { Value = addition });
+                    added = true;
+                }
+            }
+            BreadthFirstTraverse(AddConditionally);
+        }
+    }
+}
